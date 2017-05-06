54151fe6644c968b9a22d41bc44a5d36cb7d83f3
Akash Gajjar <akashgajjar8@gmail.com>
2017-05-06 08:51:23 +0530

    added random and search button

--- script/script.js
+++ script/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
-    $(".src-btn").on("click", function() {
-        console.log("search button was clicked!");
-    })
+  $(".src-btn").on("click", function(event) {
+    event.preventDefault();
+    console.log("search button was clicked!");
+    var value = $(".text-content").val();
+    if (value.length == 0) value = "Empty";
+    console.log("Query: " + value);
+
+    $.ajax({
+      url: "https://en.wikipedia.org/w/api.php?action=query&titles=" + value + "&prop=revisions&rvprop=content&format=json",
+      dataType: 'json',
+      type: 'POST',
+      headers: {
+        'Api-User-Agent': 'Example/1.0',
+        'Access-Control-Allow-Origin': *
+      },
+      success: function(data) {
+        // do something with data
+      }
+    });
+  });
 });
