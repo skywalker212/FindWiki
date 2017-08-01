0c7dc39387a7468d3949b582189933d057173ed0
Akash Gajjar <akashgajjar8@gmail.com>
2017-08-02 00:56:52 +0530

    made some styling changes and changed the API

--- script/script.js
+++ script/script.js
@@ -5,19 +5,18 @@ $(document).ready(function() {
     var value = $(".text-content").val();
     if (value.length == 0) value = "Empty";
     console.log("Query: " + value);
+    $(".search-results").html("<p class='wait text-center'><i>Please wait while we fetch the results...</i></p>");
 
     $.ajax({
-      url: "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&srsearch="+value+"&list=search",
+      url: "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&namespace=0&search="+value+"&list=search",
       dataType: 'json',
       type: 'POST',
       headers: { 'Api-User-Agent': 'Mozilla/5.0' },
       success: function(data) {
-        // do something with data
-        console.log(data);
-        var resultNo=0;
         var htmlString = "";
-        for(var i = 0;i<data.query.search.length;i++){
-          htmlString += "<div class='result'><a href='https://en.wikipedia.org/wiki/"+data.query.search[i].title +"'><h4>" +data.query.search[i].title +"</h4></a><p>"+data.query.search[i].snippet+"</p></div>";
+        for(var i = 0;i<data[1].length;i++){
+          if(data[2][i]=="") data[2][i] = "No Discription Available";
+          htmlString += "<a class='link' style='text-decoration:none' href='" +data[3][i] +"' target='_null'>"+"<div class='result'>" +"<h4>" +data[1][i] +"</h4><p align='justify'>"+data[2][i]+"</p></div></a>";
         }
         $(".search-results").html(htmlString);
       }
