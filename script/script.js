fb5081ba0859c1ae3e20e36c54b4672b037200b1
Akash Gajjar <akashgajjar8@gmail.com>
2017-08-01 19:19:16 +0530

    implemented the search function and made some changes in html and css

--- script/script.js
+++ script/script.js
@@ -7,14 +7,19 @@ $(document).ready(function() {
     console.log("Query: " + value);
 
     $.ajax({
-      url: "https://en.wikipedia.org/w/api.php?action=query&titles=" + value + "&prop=revisions&rvprop=content&format=json",
+      url: "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&srsearch="+value+"&list=search",
       dataType: 'json',
       type: 'POST',
-      headers: {
-        'Api-User-Agent': 'Example/1.0',
-      },
+      headers: { 'Api-User-Agent': 'Mozilla/5.0' },
       success: function(data) {
         // do something with data
+        console.log(data);
+        var resultNo=0;
+        var htmlString = "";
+        for(var i = 0;i<data.query.search.length;i++){
+          htmlString += "<div class='result'><a href='https://en.wikipedia.org/wiki/"+data.query.search[i].title +"'><h4>" +data.query.search[i].title +"</h4></a><p>"+data.query.search[i].snippet+"</p></div>";
+        }
+        $(".search-results").html(htmlString);
       }
     });
   });
